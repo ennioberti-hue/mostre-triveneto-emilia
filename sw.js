b1a7eaeccd80b35f67b24f3a1c7277126a3441eb
ennioberti-hue <ennioberti@gmail.com>
2026-09-23 18:09:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mostre-cache-v1';
+const CACHE_NAME = 'mostre-cache-v2';
 const APP_SHELL = [
   './',
   './index.html',
@@ -30,7 +30,7 @@ self.addEventListener('fetch', event => {
   if (url.origin !== self.location.origin) return;
 
   event.respondWith(
-    fetch(event.request)
+    fetch(event.request, {cache: 'no-store'})
       .then(response => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
